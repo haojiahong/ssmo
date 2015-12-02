dd09c495b7c6806b01743f871a6922a150ac8511
Jokerone <1548620258@qq.com>
2015-12-02 17:05:45 +0800

    搭好了springmvc加入了mybatis 的jar包，下一步结合spring和mybatis

---
+++ src/main/java/com/hjh/ssmo/model/Items.java
@@ -0,0 +1,31 @@
+package com.hjh.ssmo.model;
+
+public class Items {
+	private String name;
+	private float price;
+	private String detail;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+}
---
+++ src/main/java/com/hjh/ssmo/ssmo/oa/controller/ItemList.java
@@ -0,0 +1,42 @@
+package com.hjh.ssmo.ssmo.oa.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.hjh.ssmo.model.Items;
+
+@Controller
+public class ItemList {
+	@RequestMapping("/queryItem.action")
+	public ModelAndView queryItem() {
+		// 商品列表
+		List<Items> itemsList = new ArrayList<Items>();
+
+		Items items_1 = new Items();
+		items_1.setName("联想笔记本");
+		items_1.setPrice(6000f);
+		items_1.setDetail("ThinkPad T430 联想笔记本电脑！");
+
+		Items items_2 = new Items();
+		items_2.setName("苹果手机");
+		items_2.setPrice(5000f);
+		items_2.setDetail("iphone6苹果手机！");
+
+		itemsList.add(items_1);
+		itemsList.add(items_2);
+
+		// 创建modelAndView准备填充数据、设置视图
+		ModelAndView modelAndView = new ModelAndView();
+
+		// 填充数据
+		modelAndView.addObject("itemsList", itemsList);
+		// 视图
+		modelAndView.setViewName("hello");
+
+		return modelAndView;
+	}
+}
